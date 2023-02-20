8c447f0a5644d0062a1eea431fddea211f9219b2
andersondario <andersondario@gmail.com>
2023-02-20 14:11:45 -0300

    Refactoring

--- argocd-install/install.sh
+++ argocd-install/install.sh
@@ -7,18 +7,19 @@ istioctl install --set profile=minimal -y
 
 kubectl create namespace kong-istio
 kubectl create namespace argocd
+kubectl create namespace monitoring
 
 kubectl label namespace kong-istio istio-injection=enabled
 kubectl label namespace argocd istio-injection=enabled
+kubectl label namespace monitoring istio-injection=enabled
 
 helm install -n kong-istio kong-istio kong/kong
-helm install -n argocd argocd argocd/argo-cd --version 5.19.10 --set "server.extraArgs={ --insecure }"
+helm install -n argocd argocd ./infra/argo-cd
 
 kubectl apply -f argocd-core-projects.yaml
 kubectl apply -f argocd-core-apps.yaml
-kubectl apply -f argocd-ingress.yaml
 
 sleep 90
 
-kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d
-kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 -d
+kubectl -n argocd get secrets argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d | echo
+kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 -d | echo
